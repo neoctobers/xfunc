3f63d3759059a2558348bfb53b907a220ff6b06d
neoctobers <neoctobers@gmail.com>
2018-11-22 14:29:50 +0800

    xfunc -> xfunctions

--- setup.py
+++ setup.py
@@ -5,21 +5,19 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="xfunc",
+    name="xfunctions",
     version="0.1.3",
     author="@neoctobers",
     author_email="neoctobers@gmail.com",
     description="Some functions, maybe only for myself.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/neoctobers/xfunc",
+    url="https://github.com/neoctobers/xfunctions",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=[
-        'xprint',
-    ],
+    install_requires=[],
 )
